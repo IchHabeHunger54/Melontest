80a64f6bbfaae9381cdeb21b9eb33f4b77d8cc31
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-07-17 16:58:58 +0200

    move Database class to module.py

--- module.py
+++ module.py
@@ -2,10 +2,9 @@ import json
 from datetime import datetime
 from typing import Optional
 
+import psycopg2
 from discord import *
 from discord.ext import tasks
-
-from database import Database
 
 
 class Config:
@@ -335,3 +334,26 @@ class Module:
         except ValueError:
             return None
         return base * factor
+
+
+class Database:
+    def __init__(self, database: dict):
+        self.username = database['username']
+        self.password = database['password']
+        self.hostname = database['hostname']
+        self.database = database['database']
+        self.port = database['port']
+
+    def execute(self, query: str, *args) -> list[tuple]:
+        try:
+            with psycopg2.connect(user=self.username, password=self.password, host=self.hostname, database=self.database, port=self.port) as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(query, args)
+                    try:
+                        result = cursor.fetchall()
+                    except psycopg2.ProgrammingError:
+                        result = None
+                    connection.commit()
+                    return result
+        except (Exception, psycopg2.DatabaseError) as e:
+            print('Caught SQL Error:', e)
